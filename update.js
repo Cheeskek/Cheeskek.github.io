dbf868f6e3554b255d4cd650c057c42c081d3929
Cheeskek <61044082+Cheeskek@users.noreply.github.com>
2020-07-03 11:41:12 +0300

    Update update.js

--- update.js
+++ update.js
@@ -20,8 +20,7 @@ function draw() {
             localDelta = 0;
             speedControl = -7;
         }
-    }
-    else if (speedControl > 0) {
+    } else if (speedControl > 0) {
         if (screen == 1) {
             speed = 0.2;
             if (localDelta >= 1000) {
@@ -29,8 +28,7 @@ function draw() {
                 localDelta = 0;
             }
         }
-    }
-    else {
+    } else {
         if (screen == 1) {
             speed = 1;
             if (localDelta >= 1000) {
@@ -59,8 +57,7 @@ function draw() {
     if (speedControl >= 0) {
         fill(200, 150, 0);
         text(speedControl, 290, 580);
-    }
-    else {
+    } else {
         fill(200, 0, 0);
         text(speedControl * -1, 290, 580);
     }
@@ -72,23 +69,20 @@ function draw() {
             sounds.win.play();
             winPlay = false;
         }
-    }
-    else if (!winPlay) {
+    } else if (!winPlay) {
         winPlay = true;
     }
     if (screen == 0) {
         image(menuImg, 0, 0);
         if (menuSelect) {
             fill(255, 255, 0);
-        }
-        else {
+        } else {
             fill(255);
         }
         text('Играть', 260, 450);
         if (menuSelect) {
             fill(255);
-        }
-        else {
+        } else {
             fill(255, 255, 0);
         }
         text('Как играть', 240, 480);
@@ -100,8 +94,7 @@ function draw() {
         triangle(285, 490, 295, 490, 290, 500);
         text(' - ВЫБОР.', 300, 500);
         text('Enter - OK.', 260, 520);
-    }
-    else if (screen == -1) {
+    } else if (screen == -1) {
         image(menuImg, 0, 0);
         textSize(16);
         fill(255);
@@ -110,8 +103,9 @@ function draw() {
         text('Стреляя в разные стороны нужно попасть во второго игрока, после', 80, 390);
         text('чего у него отнимется единица здоровья (их значение можно увидеть', 50, 410);
         text('внизу экрана). Когда у противника закончатся жизни, вы победите.', 50, 430);
+        text('Также можно отбивать пули противника мечом, если сменить оружие', 80, 450)
         textSize(24);
-        text('УПРАВЛЕНИЕ', 230, 490);
+        text('УПРАВЛЕНИЕ', 230, 480);
         textSize(16);
         text('Первый игрок', 50, 490);
         text('W', 53, 520);
@@ -120,20 +114,24 @@ function draw() {
         text('I', 173, 520);
         text('J   K   L', 150, 545);
         text('Стрельба', 140, 575);
+        text('L Shift - сменить оружие', 60, 595);
         text('Второй игрок', 450, 490);
         triangle(445, 520, 455, 520, 450, 510);
         triangle(435, 535, 435, 545, 425, 540);
         triangle(445, 535, 455, 535, 450, 545);
         triangle(465, 535, 465, 545, 475, 540);
         text('Ходьба', 425, 575);
+        text('R Ctrl - сменить оружие', 385, 595);
         text('5', 545, 520);
         text('1   2   3', 525, 545);
         text('Стрельба', 515, 575);
-        text('(NumPad)', 515, 595);
-        text('Escape - пауза', 260, 530);
+        text('Num', 565, 500);
+        text('Pad', 565, 520)
+        text('Пробел / Num0 -', 260, 500);
+        text('замедление', 260, 520);
+        text('Escape - пауза', 265, 550);
         text('Нажите Enter', 265, 590);
-    }
-    else if (screen == 1) {
+    } else if (screen == 1) {
         player[0].move();
         player[1].move();
         if (fire[0] == 0 && health[0] != 0 && health[1] != 0) {
@@ -145,29 +143,24 @@ function draw() {
                 var shft = false;
                 if (player[0].powerUp == -1) {
                     bullet = new WideBullet(0);
-                }
-                else if (player[0].powerUp == 1) {
+                } else if (player[0].powerUp == 1) {
                     player[0].swordRight = (player[0].swordRight) ? (false) : (true);
                     bullet = new Sword(0);
                     shft = true;
-                }
-                else {
+                } else {
                     bullet = new Bullet(0);
                 }
                 if (shft) {
                     bullets[0].unshift(bullet)
-                }
-                else {
+                } else {
                     bullets[0].push(bullet);
                 }
                 if (speed == 0.2) {
                     fire[0] = 35;
-                }
-                else {
+                } else {
                     fire[0] = 10;
                 }
-            }
-            else if (pressed.fireDown[0]) {
+            } else if (pressed.fireDown[0]) {
                 dir[0] = 'down';
                 bulX[0] = player[0].x + 5;
                 bulY[0] = player[0].y + 5;
@@ -175,29 +168,24 @@ function draw() {
                 var shft = false;
                 if (player[0].powerUp == -1) {
                     bullet = new WideBullet(0);
-                }
-                else if (player[0].powerUp == 1) {
+                } else if (player[0].powerUp == 1) {
                     player[0].swordRight = (player[0].swordRight) ? (false) : (true);
                     bullet = new Sword(0);
                     shft = true;
-                }
-                else {
+                } else {
                     bullet = new Bullet(0);
                 }
                 if (shft) {
                     bullets[0].unshift(bullet);
-                }
-                else {
+                } else {
                     bullets[0].push(bullet);
                 }
                 if (speed == 0.2) {
                     fire[0] = 35;
-                }
-                else {
+                } else {
                     fire[0] = 10;
                 }
-            }
-            else if (pressed.fireRight[0]) {
+            } else if (pressed.fireRight[0]) {
                 dir[0] = 'right';
                 bulX[0] = player[0].x + 5;
                 bulY[0] = player[0].y + 5;
@@ -205,29 +193,24 @@ function draw() {
                 var shft = false;
                 if (player[0].powerUp == -1) {
                     bullet = new WideBullet(0);
-                }
-                else if (player[0].powerUp == 1) {
+                } else if (player[0].powerUp == 1) {
                     player[0].swordRight = (player[0].swordRight) ? (false) : (true);
                     bullet = new Sword(0);
                     shft = true;
-                }
-                else {
+                } else {
                     bullet = new Bullet(0);
                 }
                 if (shft) {
                     bullets[0].unshift(bullet);
-                }
-                else {
+                } else {
                     bullets[0].push(bullet);
                 }
                 if (speed == 0.2) {
                     fire[0] = 35;
-                }
-                else {
+                } else {
                     fire[0] = 10;
                 }
-            }
-            else if (pressed.fireLeft[0]) {
+            } else if (pressed.fireLeft[0]) {
                 dir[0] = 'left';
                 bulX[0] = player[0].x + 5;
                 bulY[0] = player[0].y + 5;
@@ -235,34 +218,28 @@ function draw() {
                 var shft = false;
                 if (player[0].powerUp == -1) {
                     bullet = new WideBullet(0);
-                }
-                else if (player[0].powerUp == 1) {
+                } else if (player[0].powerUp == 1) {
                     player[0].swordRight = (player[0].swordRight) ? (false) : (true);
                     bullet = new Sword(0);
                     shft = true;
-                }
-                else {
+                } else {
                     bullet = new Bullet(0);
                 }
                 if (shft) {
                     bullets[0].unshift(bullet);
-                }
-                else {
+                } else {
                     bullets[0].push(bullet);
                 }
                 if (speed == 0.2) {
                     fire[0] = 35;
-                }
-                else {
+                } else {
                     fire[0] = 10;
                 }
             }
-        }
-        else {
+        } else {
             if (pressed.fireDown[0] || pressed.fireLeft[0] || pressed.fireRight[0] || pressed.fireUp[0]) {
                 fire[0]--;
-            }
-            else {
+            } else {
                 fire[0] = 0;
             }
         }
@@ -274,113 +251,94 @@ function draw() {
                 var bullet;
                 if (player[1].powerUp == -1) {
                     bullet = new WideBullet(1);
-                }
-                else if (player[1].powerUp == 1) {
+                } else if (player[1].powerUp == 1) {
                     player[1].swordRight = (player[1].swordRight) ? (false) : (true);
                     bullet = new Sword(1);
-                }
-                else {
+                } else {
                     bullet = new Bullet(1);
                 }
                 bullets[1].push(bullet);
                 if (speed == 0.2) {
                     fire[1] = 35;
-                }
-                else {
+                } else {
                     fire[1] = 10;
                 }
-            }
-            else if (pressed.fireDown[1]) {
+            } else if (pressed.fireDown[1]) {
                 dir[1] = 'down';
                 bulX[1] = player[1].x + 5;
                 bulY[1] = player[1].y + 5;
                 var bullet;
                 if (player[1].powerUp == -1) {
                     bullet = new WideBullet(1);
-                }
-                else if (player[1].powerUp == 1) {
+                } else if (player[1].powerUp == 1) {
                     player[1].swordRight = (player[1].swordRight) ? (false) : (true);
                     bullet = new Sword(1);
-                }
-                else {
+                } else {
                     bullet = new Bullet(1);
                 }
                 bullets[1].push(bullet);
                 if (speed == 0.2) {
                     fire[1] = 35;
-                }
-                else {
+                } else {
                     fire[1] = 10;
                 }
-            }
-            else if (pressed.fireRight[1]) {
+            } else if (pressed.fireRight[1]) {
                 dir[1] = 'right';
                 bulX[1] = player[1].x + 5;
                 bulY[1] = player[1].y + 5;
                 var bullet;
                 if (player[1].powerUp == -1) {
                     bullet = new WideBullet(1);
-                }
-                else if (player[1].powerUp == 1) {
+                } else if (player[1].powerUp == 1) {
                     player[1].swordRight = (player[1].swordRight) ? (false) : (true);
                     bullet = new Sword(1);
-                }
-                else {
+                } else {
                     bullet = new Bullet(1);
                 }
                 bullets[1].push(bullet);
                 if (speed == 0.2) {
                     fire[1] = 35;
-                }
-                else {
+                } else {
                     fire[1] = 10;
                 }
-            }
-            else if (pressed.fireLeft[1]) {
+            } else if (pressed.fireLeft[1]) {
                 dir[1] = 'left';
                 bulX[1] = player[1].x + 5;
                 bulY[1] = player[1].y + 5;
                 var bullet;
                 if (player[1].powerUp == -1) {
                     bullet = new WideBullet(1);
-                }
-                else if (player[1].powerUp == 1) {
+                } else if (player[1].powerUp == 1) {
                     player[1].swordRight = (player[1].swordRight) ? (false) : (true);
                     bullet = new Sword(1);
-                }
-                else {
+                } else {
                     bullet = new Bullet(1);
                 }
                 bullets[1].push(bullet);
                 if (speed == 0.2) {
                     fire[1] = 35;
-                }
-                else {
+                } else {
                     fire[1] = 10;
                 }
             }
-        }
-        else {
+        } else {
             if (pressed.fireDown[1] || pressed.fireLeft[1] || pressed.fireRight[1] || pressed.fireUp[1]) {
                 fire[1]--;
-            }
-            else {
+            } else {
                 fire[1] = 0;
             }
         }
         for (var i = bullets[0].length - 1; i >= 0; i--) {
             if (bullets[0][i].toDelete) {
                 bullets[0].splice(i, 1);
-            }
-            else {
+            } else {
                 bullets[0][i].move();
             }
         }
         for (var i = bullets[1].length - 1; i >= 0; i--) {
             if (bullets[1][i].toDelete) {
                 bullets[1].splice(i, 1);
-            }
-            else {
+            } else {
                 bullets[1][i].move();
             }
         }
@@ -390,22 +348,19 @@ function draw() {
         if (inv[1] > 0) {
             inv[1]--;
         }
-    }
-    else if (screen == 2) {
+    } else if (screen == 2) {
         textSize(24);
         fill(255);
         text('Пауза', 265, 300);
         if (menuSelect) {
             fill(255, 255, 0);
-        }
-        else {
+        } else {
             fill(255);
         }
         text('Заново', 260, 450);
         if (menuSelect) {
             fill(255);
-        }
-        else {
+        } else {
             fill(255, 255, 0);
         }
         text('Выход', 265, 480);
@@ -417,8 +372,7 @@ function draw() {
         triangle(285, 490, 295, 490, 290, 500);
         text(' - ВЫБОР.', 300, 500);
         text('Enter - OK.', 260, 520);
-    }
-    else {
+    } else {
         pressed = { up: [false, false], down: [false, false], left: [false, false], right: [false, false], fireUp: [false, false], fireDown: [false, false], fireLeft: [false, false], fireRight: [false, false] };
         inv[0] = 0;
         inv[1] = 0;
@@ -433,8 +387,7 @@ function draw() {
             fill(200, 0, 200);
             textSize(40);
             text('Ничья!!1!', 160, 100);
-        }
-        else if (health[0] <= 0) {
+        } else if (health[0] <= 0) {
             dead = true;
             player[0].x = -10;
             player[0].y = -10;
@@ -443,8 +396,7 @@ function draw() {
             fill(200, 0, 0);
             textSize(40);
             text('Второй игрок победил!', 100, 100);
-        }
-        else if (health[1] <= 0) {
+        } else if (health[1] <= 0) {
             dead = true;
             player[1].x = -10;
             player[1].x = -10;
@@ -457,15 +409,13 @@ function draw() {
         textSize(24);
         if (menuSelect) {
             fill(255, 255, 0);
-        }
-        else {
+        } else {
             fill(255);
         }
         text('Заново', 260, 450);
         if (menuSelect) {
             fill(255);
-        }
-        else {
+        } else {
             fill(255, 255, 0);
         }
         text('Выход', 265, 480);
@@ -494,8 +444,7 @@ function keyPressed() {
     if (keyCode == 27) {
         if (screen == 1) {
             screen = 2;
-        }
-        else if (screen == 2) {
+        } else if (screen == 2) {
             screen = 1;
         }
         if (screen > 0) {
@@ -503,13 +452,11 @@ function keyPressed() {
             sounds.escape.play();
         }
         menuSelect = true;
-    }
-    else if (keyCode == 38 || keyCode == 40 || keyCode == 87 || keyCode == 83) {
+    } else if (keyCode == 38 || keyCode == 40 || keyCode == 87 || keyCode == 83) {
         if (screen != 1) {
             if (menuSelect) {
                 menuSelect = false;
-            }
-            else {
+            } else {
                 menuSelect = true;
             }
             if (screen != -1) {
@@ -545,38 +492,30 @@ function keyPressed() {
                 choose = Math.floor(Math.random() * (3));
                 if (choose == 0) {
                     walls = walls0;
-                }
-                else if (choose == 1) {
+                } else if (choose == 1) {
                     walls = walls1;
-                }
-                else if (choose == 2) {
+                } else if (choose == 2) {
                     walls = walls2;
                 }
-            }
-            else {
+            } else {
                 screen = 0;
                 localDelta = 0;
             }
-        }
-        else if (screen == 0) {
+        } else if (screen == 0) {
             if (menuSelect) {
                 screen = 1;
                 choose = Math.floor(Math.random() * (3));
                 if (choose == 0) {
                     walls = walls0;
-                }
-                else if (choose == 1) {
+                } else if (choose == 1) {
                     walls = walls1;
-                }
-                else if (choose == 2) {
+                } else if (choose == 2) {
                     walls = walls2;
                 }
-            }
-            else {
+            } else {
                 screen = -1;
             }
-        }
-        else if (screen == -1) {
+        } else if (screen == -1) {
             screen = 0;
             localDelta = 0;
         }
